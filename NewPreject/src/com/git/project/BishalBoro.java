96dad08a9b8d2280187888fdc3eeb7267b960127
RonyJH <munjurulh@yahoo.com>
2014-12-01 21:01:49 -0500

    add file

---
+++ NewPreject/src/com/git/project/BishalBoro.java
@@ -0,0 +1,16 @@
+package com.git.project;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BishalBoro 
+{
+
+	public static void main(String[] args) 
+	{
+		FirefoxDriver k= new FirefoxDriver();
+		k.get("http://www.facebook.com");
+		// TODO Auto-generated method stub
+
+	}
+
+}
